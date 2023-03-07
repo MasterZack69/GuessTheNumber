795b443774c94c8853f2c922101d396c92eee57b
Zackie <102979057+MasterZack69@users.noreply.github.com>
2023-03-07 12:32:30 +0530

    Add files via upload

--- guess.cpp
+++ guess.cpp
@@ -1,35 +1,43 @@
 #include <iostream>
 #include <cstdlib>
 
-int main() {
-        std::cout << "\nGuess The Number between 1-20!!!\n";         
-        std::cout << "You have got only 3 chances!\n";
+int main()
+{
+	std::cout << "\nGuess The Number between 1-20!!!\n";
+	std::cout << "You have got only 3 chances!\n";
 
-        srand((unsigned) time(NULL));
-        int num = 1 + (rand() % 20);
-        int x;                                                                                                                      for(int i = 0; i < 3; i++){
-                std::cout << "Guess the Number : ";
-                std::cin >> x;
+	srand((unsigned)time(NULL));
+	int num = 1 + (rand() % 20);
+	int x;
 
-                if(i == 2 && x != num) {                                              std::cout << "You Lost!!!\n";
-                        std::cout << "It was " << num << "\n\n";
-                        break;
-                }
+	for (int i = 0; i < 3; i++)
+	{
+		std::cout << "Guess the Number : ";
+		std::cin >> x;
 
-                if (x > num){
-                        std::cout << "Too high!\n";
-                        continue;
-                }
+		if (i == 2 && x != num)
+		{
+			std::cout << "You Lost!!!\n";
+			std::cout << "It was " << num << "\n\n";
+			break;
+		}
 
-                if (x < num){
-                         std::cout << "Too low! \n";
-                         continue;
-                }
+		if (x > num)
+		{
+			std::cout << "Too high!\n";
+			continue;
+		}
 
-                if (x == num){
-                          std::cout << "Correct!!!! \n\n";
-                          break;
-                }
-        }
+		if (x < num)
+		{
+			std::cout << "Too low! \n";
+			continue;
+		}
 
+		if (x == num)
+		{
+			std::cout << "Correct!!!! \n\n";
+			break;
+		}
+	}
 }
